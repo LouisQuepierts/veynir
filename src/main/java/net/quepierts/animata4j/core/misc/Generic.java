7a4cf081edd50eeedd4e88e3912a78a84bb30782
Louis_Quepierts <LouisQuepierts@outlook.com>
2025-08-15 17:49:06 +0800

    add BinarySearch utility class
    add doc comments for the following classes:
    BinarySearch
    Generic
    UnsafeUtil

--- src/main/java/net/quepierts/animata4j/core/misc/Generic.java
+++ src/main/java/net/quepierts/animata4j/core/misc/Generic.java
@@ -1,12 +1,32 @@
 package net.quepierts.animata4j.core.misc;
 
+import lombok.experimental.UtilityClass;
+
+/**
+ * A utility class that provides generic type operations.
+ */
+@UtilityClass
 @SuppressWarnings("unused")
 public class Generic {
+    /**
+     * Casts an object to the specified type without type checking.
+     *
+     * @param o the object to cast
+     * @param <T> the target type
+     * @return the object cast to the target type
+     */
     @SuppressWarnings("unchecked")
     public static <T> T cast(Object o) {
         return (T) o;
     }
 
+    /**
+     * Creates a new array of the specified size with Object elements.
+     *
+     * @param size the size of the array
+     * @param <T> the type of elements in the array
+     * @return a new array of the specified size
+     */
     @SuppressWarnings("unchecked")
     public static <T> T[] newArray(int size) {
         return (T[]) new Object[size];
--- src/main/java/net/quepierts/animata4j/core/misc/UnsafeUtil.java
+++ src/main/java/net/quepierts/animata4j/core/misc/UnsafeUtil.java
@@ -1,9 +1,14 @@
 package net.quepierts.animata4j.core.misc;
 
+import lombok.experimental.UtilityClass;
 import sun.misc.Unsafe;
 
 import java.lang.reflect.Field;
 
+/**
+ * A utility class that provides access to the Unsafe operations and memory management functions.
+ */
+@UtilityClass
 @SuppressWarnings("unused")
 public class UnsafeUtil {
     public static final Unsafe UNSAFE;
@@ -14,16 +19,34 @@ public class UnsafeUtil {
     public static final long FLOAT_ARRAY_OFFSET;
     public static final long DOUBLE_ARRAY_OFFSET;
 
+    /**
+     * Allocates memory of the specified size and initializes it with zeros.
+     *
+     * @param size the size of memory to allocate
+     * @return the address of the allocated memory
+     */
     public static long malloc(long size) {
         return UnsafeUtil.malloc(size, (byte) 0);
     }
 
+    /**
+     * Allocates memory of the specified size and initializes it with the given value.
+     *
+     * @param size the size of memory to allocate
+     * @param value the initial value to set for the allocated memory
+     * @return the address of the allocated memory
+     */
     public static long malloc(long size, byte value) {
         long address = UNSAFE.allocateMemory(size);
         UNSAFE.setMemory(address, size, value);
         return address;
     }
 
+    /**
+     * Frees the memory at the specified address.
+     *
+     * @param address the address of the memory to free
+     */
     public static void free(long address) {
         UNSAFE.freeMemory(address);
     }
